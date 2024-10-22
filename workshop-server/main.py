f82c40c67f1f6d5d313976ce2b2f0265db85ea0c
Taah <admin@taah.dev>
2024-10-22 13:07:44 -0700

    Update workshop 1 files

--- workshop-server/main.py
+++ workshop-server/main.py
@@ -10,7 +10,7 @@ app.debug = True
 @app.route("/", methods=["GET"])
 def get_test():
     # Get Parameters usng request.args (dict)
-    return "Test"
+    return json.dumps(todos)
 
 @app.route("/", methods=["POST"])
 def post_test():
@@ -18,7 +18,25 @@ def post_test():
     # Check Content Type
     # Check if they specified JSON data
     # Check if they provided a valid operation ("add" or "remove")
-    
-    return "ToDo Updated"
+    if request.content_type != "application/json":
+        return "Specify header Content-Type=application/json\n"
+    data = {}
+    try:
+        data = request.get_json()
+    except:
+        if not data:
+            return "Invalid Option, provide JSON data that looks similar to {'add': 'adding', 'remove': 'removing'}\n"
+
+    valid_option = False
+    if "add" in data:
+        todos.append(data["add"])
+        valid_option = True
+    if "remove" in data:
+        if data["remove"] in todos:
+            todos.remove(data["remove"])
+        valid_option = True
+    if not valid_option:
+        return "Invalid Option, provide JSON data that looks similar to {'add': 'adding', 'remove': 'removing'}\n"
+    return "ToDo Updated\n"
 
 app.run(port=3001)
